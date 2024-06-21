f7e2d550ffc92d279968c8cf2bd81f89546bd957
Sefa Bulak <zckhtln@icloud.com>
2024-06-21 19:12:15 +0200

    added userAvatar component

--- app.vue
+++ app.vue
@@ -85,11 +85,7 @@ useHead({
       <div class="form">
         <FormTitle />
         <div :class="['join', { 'input-focused': isInputFocused }]">
-          <div v-if="!username || fetchError" class="avatar none"></div>
-          <div v-else-if="isLoading" class="avatar">
-            <Icon name="svg-spinners:bars-rotate-fade" size="32" />
-          </div>
-          <img v-else-if="userData" :src="userData.avatar_url" class="avatar" />
+          <UserAvatar :username="username" :fetchError="fetchError" :isLoading="isLoading" :userData="userData" />
           <input v-model="username" type="text" placeholder="Type your Github username" @focus="isInputFocused = true" @blur="isInputFocused = false" @keyup.enter="sendInvite" />
           <div class="submit" @click="sendInvite">
             <span class="submit-text">join us</span>
@@ -209,23 +205,6 @@ body {
   box-shadow: 0px 9px 30px -15px rgb(150 243 135 / 40%);
   background: rgb(48 83 42 / 15%);
   backdrop-filter: blur(3px);
-  transition-property: all;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 200ms;
-}
-.avatar {
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  height: 40px;
-  width: 40px;
-  border-radius: 99px;
-  box-shadow: 0px 3px 5px #000;
-}
-.none {
-  background-color: #95f386;
-  color: #000;
-  font-size: 8px;
   transition-property: all;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 200ms;
